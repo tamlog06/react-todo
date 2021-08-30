c0f7d117c8b57d802414da93e6af6db1aa2a0718
tam1006 <tamusyun1006@gmail.com>
2021-08-31 00:36:29 +0900

    first init

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App tasks={DATA} />,
   document.getElementById('root')
 );
 
